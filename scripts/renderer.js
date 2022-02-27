e9f390fef29fa904a0b8818323ca76cba5a5c219
Hieu Le <hieule@Hieus-MacBook-Air.local>
2022-02-26 20:11:40 -0600

    almost done

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -46,22 +46,40 @@ class Renderer {
 
     // ctx:          canvas context
     drawSlide0(ctx) {
-        
+        this.drawRectangle({x: 200, y: 150}, {x: 600, y: 500}, [255, 0, 0, 255], ctx);
     }
 
     // ctx:          canvas context
     drawSlide1(ctx) {
-
+        this.drawCircle({x: 400, y: 300}, 200, [255, 0, 0, 255], ctx);
     }
 
     // ctx:          canvas context
     drawSlide2(ctx) {
-
+        this.drawBezierCurve({x: 200, y: 300}, {x: 300, y: 600}, {x: 600, y: 500}, {x: 600, y: 200}, [255, 0, 0, 255], ctx);
     }
 
     // ctx:          canvas context
     drawSlide3(ctx) {
+        var color = [255, 0, 0, 255];
+        //H
+        this.drawRectangle({x: 100, y: 200}, {x: 100, y: 400}, color, ctx);
+        this.drawRectangle({x: 100, y: 300}, {x: 200, y: 300}, color, ctx);
+        this.drawRectangle({x: 200, y: 200}, {x: 200, y: 400}, color, ctx);
+        //I
+        this.drawRectangle({x: 250, y: 200}, {x: 250, y: 380}, color, ctx);
+        this.drawCircle({x: 250, y: 390}, 20, color, ctx);
 
+        //E
+        this.drawRectangle({x: 300, y: 200}, {x: 300, y: 400}, color, ctx);
+        this.drawRectangle({x: 400, y: 300}, {x: 300, y: 300}, color, ctx);
+        this.drawRectangle({x: 400, y: 200}, {x: 300, y: 200}, color, ctx);
+        this.drawRectangle({x: 400, y: 400}, {x: 300, y: 400}, color, ctx);
+        
+        //U
+        this.drawRectangle({x: 450, y: 230}, {x: 450, y: 400}, color, ctx);
+        this.drawRectangle({x: 550, y: 230}, {x: 550, y: 400}, color, ctx);
+        this.drawBezierCurve({x: 450, y: 230}, {x: 480, y: 200}, {x: 550, y: 200}, {x: 550, y: 230}, color, ctx);
     }
 
     // left_bottom:  object ({x: __, y: __})
@@ -69,7 +87,13 @@ class Renderer {
     // color:        array of int [R, G, B, A]
     // ctx:          canvas context
     drawRectangle(left_bottom, right_top, color, ctx) {
-        
+        var left_top =  {x: left_bottom.x, y: right_top.y};
+        var right_bottom = {x: right_top.x, y: left_bottom.y};
+        var points = [left_bottom, left_top, right_top, right_bottom];
+        this.drawShape(points, color, ctx);
+        if(this.show_points) {
+            this.drawVertices(points, color, ctx);
+        }
     }
 
     // center:       object ({x: __, y: __})
@@ -77,7 +101,22 @@ class Renderer {
     // color:        array of int [R, G, B, A]
     // ctx:          canvas context
     drawCircle(center, radius, color, ctx) {
-        
+        var points = [];
+        var curAngle = 0;
+        var eachAngle = 360/this.num_curve_sections;
+        while(curAngle < 360) {
+            var x = center.x + radius * Math.cos(this.toRadians(curAngle));
+            var y = center.y + radius * Math.sin(this.toRadians(curAngle));
+            points.push({x, y});
+            curAngle += eachAngle;
+        }
+        this.drawShape(points, color, ctx);
+        //Draw vertices
+        if(this.show_points) {
+            for(let i = 0; i < points.length - 1; i++) {
+                this.drawSmallRect(points[i], color, ctx);
+            }
+        }
     }
 
     // pt0:          object ({x: __, y: __})
@@ -87,7 +126,19 @@ class Renderer {
     // color:        array of int [R, G, B, A]
     // ctx:          canvas context
     drawBezierCurve(pt0, pt1, pt2, pt3, color, ctx) {
-        
+        var points = [];
+        var eachStep = 1/this.num_curve_sections;
+        var curT = 0;
+        while(curT < 1) {
+            var x = Math.pow(1-curT, 3) * pt0.x + 3 * Math.pow(1-curT, 2) * curT * pt1.x + 3 * (1-curT) * Math.pow(curT, 2) * pt2.x + Math.pow(curT, 3) * pt3.x;
+            var y = Math.pow(1-curT, 3) * pt0.y + 3 * Math.pow(1-curT, 2) * curT * pt1.y + 3 * (1-curT) * Math.pow(curT, 2) * pt2.y + Math.pow(curT, 3) * pt3.y;
+            points.push({x, y});
+            curT += eachStep;
+        }
+        this.drawShape(points, color, ctx, true);
+        if(this.show_points) {
+            this.drawVertices(points, color, ctx, true);
+        }
     }
 
     // pt0:          object ({x: __, y: __})
@@ -101,5 +152,38 @@ class Renderer {
         ctx.moveTo(pt0.x, pt0.y);
         ctx.lineTo(pt1.x, pt1.y);
         ctx.stroke();
+    }
+
+    drawShape(points, color, ctx, isBezier) {
+        if(points.length !== 0) {
+            var clone = points;
+            if(!isBezier) {
+                clone.push(clone[0]);
+            }
+            for(let i = 0; i < clone.length - 1; i++) {
+                this.drawLine(clone[i], clone[i+1], color, ctx);
+            }
+        }
+    }
+    
+    drawVertices(points, color, ctx, isBezier) {
+        var len = points.length;
+        if(!isBezier) len = len - 1;
+        for(let i = 0; i < len; i++) {
+            this.drawSmallRect(points[i], color, ctx);
+        }
+    }
+
+    drawSmallRect(point, color, ctx) {
+        var left_bottom = {x: point.x - 10, y: point.y - 10};
+        var right_top = {x: point.x + 10, y: point.y + 10};
+        var left_top =  {x: left_bottom.x, y: right_top.y};
+        var right_bottom = {x: right_top.x, y: left_bottom.y};
+        var points = [left_bottom, left_top, right_top, right_bottom];
+        this.drawShape(points, color, ctx);
+    }
+
+    toRadians(degrees) {
+        return degrees * (Math.PI/180);
     }
 };
